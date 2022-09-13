41f9d7191d0c3a4ea05a09afcac960ce64f9f694
Emmanuel Pius <emmCode4495@gmail.com>
2022-09-13 20:57:49 +0100

    Created the structured packages and the nnavigation screens

---
+++ app/src/main/java/com/example/weatherapp/navigation/WeatherScreens.kt
@@ -0,0 +1,10 @@
+package com.example.weatherapp.navigation
+
+enum class WeatherScreens {
+    SplashScreen,
+    MainScreen,
+    AboutScreen,
+    FavouriteScreen,
+    SearchScreen,
+    SettingsScreen
+}
---
+++ app/src/main/java/com/example/weatherapp/screens/WeatherSplashScreen.kt
@@ -0,0 +1,11 @@
+package com.example.weatherapp.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+
+@Composable
+fun WeatherSplashScreen(navController: NavController){
+    Text(text = "This is the Splash Screen")
+}
